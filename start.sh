54da69485e9cb3b7bf2dd2478efc72ccee588b3f
Hyperloop <33641935+hyperloop-rails@users.noreply.github.com>
2019-01-24 15:01:47 -0600

    Delete start.sh

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-IP=$(ifconfig en0 | grep inet | awk '$1=="inet" {print $2}')
-docker run -it \
--e DISPLAY=$IP:0 \
---privileged \
--p 127.0.0.1:3000:3000 \
--v "${HOME}/.Xauthority:/root/.Xauthority" \
--v /tmp/.X11-unix:/tmp/.X11-unix \
-panorama/chrome:fifth
